44a857085c99ff39b79020172eff4f0d5778a119
Nicolas Walthéry <nicolaswalthery@protonmail.com>
2019-05-20 12:33:22 +0200

    Add files via upload

---
+++ Random_Number_Generator/main.cpp
@@ -0,0 +1,37 @@
+/*=====================================================================================
+=--------------------Program useful to emulate a 100 sided dice-----------------------=
+=====================================================================================*/
+#include <stdio.h>
+#include <stdlib.h> // Include the function srand
+#include <time.h>
+
+int main ()
+{
+   int  RandNumber, //Variable where a stock a randomly generated number.
+        cptr=0,
+        theChosenOne=0, //At the end of the program, i'll choose one cell from the chart.
+        Stock_RandNumber[10]; //This is the chart in which is stored the randomly generated numbers.
+   char continuer;
+
+  /* initialize random seed always at the beginning of the program */
+  srand (time(NULL));
+
+   printf("Chart of random numbers in which it'll pick a number:\n");
+  do
+  {
+     for(cptr=0;cptr<10;cptr++)
+     {
+         RandNumber = rand() % 100 + 1;
+         printf("%d ", RandNumber);
+         Stock_RandNumber[cptr]=RandNumber;
+     }
+
+      theChosenOne=rand() % 10; //Here to choose which number to pick randomly from the chart Stock_Rand_number.
+
+      printf("\n\nThe random number picked is %d\n\n", Stock_RandNumber[theChosenOne]);
+
+      printf("Continue ? (y/*) ");
+      scanf(" %c", &continuer);
+  }
+  while(continuer == 'y');
+}
